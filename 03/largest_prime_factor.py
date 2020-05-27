58c11da5d110ee7a067818adb08bb12ec144bb3b
joshua <josh.moors29@gmail.com>
2020-05-27 19:42:09 +0800

    removed unnecessary lines

--- 03/largest_prime_factor.py
+++ 03/largest_prime_factor.py
@@ -1,7 +1,6 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-
 limit = 10000
 target = 600851475143
 factor_list = []
@@ -9,24 +8,18 @@ prime_list = [2]
 num_is_prime = True
 
 for num in range(3, limit):
-
     num_is_prime = True
-
     for prime in prime_list:
-
         if num % prime == 0:
             # not a prime number, as a prime number is a factor
             num_is_prime = False
             break
-
     # finished the for loop, 'num' must be prime
     if num_is_prime:
         prime_list.append(num)
-
         if target % num == 0:
             factor_list.append(num)
         # break
-
 product_factors = 1
 for elem in factor_list:
     product_factors *= elem
