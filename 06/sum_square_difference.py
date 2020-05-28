4277a6be0bd8927c6ad14ae9592f8d4b62939c8f
joshua <josh.moors29@gmail.com>
2020-05-28 22:18:27 +0800

    solution problem 6

--- 06/sum_square_difference.py
+++ 06/sum_square_difference.py
@@ -1,1 +1,12 @@
 
+sum_squares = 0
+square_sum = 0
+
+for i in range(1, 101):
+    sum_squares += i**2
+
+square_sum = (50*101)**2
+
+print(square_sum)
+print(sum_squares)
+print(square_sum - sum_squares)
