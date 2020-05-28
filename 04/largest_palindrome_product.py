a405fafc73e9b2b31b60ec1ade40b130007ed46b
joshua <josh.moors29@gmail.com>
2020-05-28 11:59:53 +0800

    solution problem 4

--- 04/largest_palindrome_product.py
+++ 04/largest_palindrome_product.py
@@ -4,3 +4,15 @@
 # product of two 2-digit numbers is 9009 = 91 × 99. 
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
+three_digits = [*range(1000, 99, -1)]
+
+num_found = False
+for x in three_digits:
+    for y in three_digits:
+        product = str(x * y)
+        if product == product[::-1]:
+            num_found = True
+            print(product)
+            break
+    if num_found:
+        break
