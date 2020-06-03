2fa2e4d69f6864e0070580753ef5d77b82eda0b9
joshua <josh.moors29@gmail.com>
2020-06-03 21:27:51 +0800

    changed problem 14 to solve with dicts() to increase speed - minimal change however

--- 14/longest_collatz.py
+++ 14/longest_collatz.py
@@ -1,25 +1,68 @@
+# The following iterative sequence is defined for the set of positive integers:
+#
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
+#
+# Using the rule above and starting with 13, we generate the following sequence:
+#
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it
+# has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+#
+# Which starting number, under one million, produces the longest chain?
+#
+# NOTE: Once the chain starts the terms are allowed to go above one million.
+import datetime
 
-max_chain = (0, 0)
-chain_length = 0
+begin_time = datetime.datetime.now()
+
 start_n = 1000000
+chain_length = 0
+max_chain = (start_n, chain_length)
 
-while start_n > 0:
-    n = start_n
-    while n != 1:
-        chain_length += 1
-        if n % 2 == 0:
-            # even number
-            n = n // 2
+
+def collatz(number):
+    """Returns the next Collatz number in the sequence"""
+
+    if number == 1:
+        return 1
+
+    if number % 2 == 0:
+        # number must be even
+        return number // 2
+    else:
+        return 3 * number + 1
+
+
+limit = 1000000
+# all_chains = {num : num_terms_in_collatz_chain}
+all_chains = {1: 1, 2: 2}
+i = 1
+
+while i < limit:
+    if i in all_chains.keys():
+        print('already exists:\t', i, 'chain count: ', all_chains[i])
+        i += 1
+        continue
+
+    seq_num = i
+    current_collatz_chain = {i: 1}
+
+    while seq_num != 1:
+        if collatz(seq_num) in all_chains.keys():
+            current_collatz_chain[seq_num] = 0
+            for key in current_collatz_chain.keys():
+                all_chains[key] = current_collatz_chain[key] + all_chains[collatz(seq_num)] + 1
+            break
         else:
-            # odd number
-            n = 3 * n + 1
-    chain_length = chain_length + 1
+            current_collatz_chain[seq_num] = 0
+            for key in current_collatz_chain.keys():
+                current_collatz_chain[key] += 1
+            seq_num = collatz(seq_num)
 
-    print('starting number: ', start_n, 'chain length: ', chain_length)
-    if max_chain[1] < chain_length:
-        max_chain = start_n, chain_length
+    current_collatz_chain = {}
+    print('new number:\t', i, 'chain count: ', all_chains[i])
+    i += 1
 
-    chain_length = 0
-    start_n -= 1
-
-print('max chain = ', max_chain)
+max_key = max(all_chains, key=all_chains.get)
+print(max_key, all_chains[max_key])
