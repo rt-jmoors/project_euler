5c1bcb540ce911b5bcb90dac2b3dbe3cd2e5fc24
joshua <josh.moors29@gmail.com>
2020-06-07 12:47:11 +0800

    solution to problem18 - maximum path sum

---
+++ 18/maximum_path_sum.py
@@ -0,0 +1,41 @@
+
+from triangle_modules import *
+
+triangle_numbers = """
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+"""
+
+nums = make_list_integers(triangle_numbers)
+triangle = make_triangle(nums)
+
+
+# Initial conditions to find all path sum combinations
+output = sum_two_rows(triangle[0], triangle[1])
+i = 2
+
+while i < len(triangle):
+
+    output = sum_two_rows(output, triangle[i])
+    i += 1
+
+# print(output)
+
+max_paths = []
+for combo in output:
+    max_paths.append(max(combo))
+
+print('Max path sum = ', max(max_paths))
