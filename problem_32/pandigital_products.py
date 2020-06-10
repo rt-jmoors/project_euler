b61407e3dad1ae8388c1d16a30f984c152deee33
joshua <josh.moors29@gmail.com>
2020-06-10 21:58:46 +0800

    solution to #32 pandigital products

--- problem_32/pandigital_products.py
+++ problem_32/pandigital_products.py
@@ -1,4 +1,3 @@
-
 def is_pandigital(num):
     """Determines if a number is pandigital or not
 
@@ -13,3 +12,35 @@ def is_pandigital(num):
         elif str(num).count(digit) > 1:
             return False
     return True
+
+
+def is_product(mult1, mult2, product):
+    if mult1 * mult2 == product:
+        return True
+    return False
+
+
+import itertools
+
+strng = '123456789'
+
+perms = []
+for i in itertools.permutations(list(strng)):
+    perms.append(''.join(i))
+
+products = []
+for x in perms:
+    for i in range(0, len(x) - 2):
+        product = int(x[-1-i:])
+        for j in range(0, len(x) - 2 - i):
+            multiplicand = int(x[0:j+1])
+            multiplier = int(x[j+1:-1-i])
+            if is_product(multiplicand, multiplier, product):
+                print(multiplicand, '*', multiplier, '=', product)
+                if product not in products:
+                    products.append(product)
+
+print('Sum of pandigital products =', sum(products))
+
+
+
